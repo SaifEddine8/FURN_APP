944d597748f96589966b23b620bc6b8f12a9ccc5
SaifEddine8 <saifbnyslamh12@gmail.com>
2026-06-20 12:39:06 +0300

    Add warning about Kotlin Gradle Plugin migration
    Added a warning comment regarding Kotlin Gradle Plugin migration for future Flutter versions.

--- youtext/lib/main.dart
+++ youtext/lib/main.dart
@@ -11,5 +11,16 @@ void main() {
 
 
 
+/*
 
+WARNING: Your Android app project: app located at: C:\Users\USER\Downloads\FURN_APP-main\FURN_APP-main\youtext\android\app\build.gradle.kts
+applies the Kotlin Gradle Plugin, which will cause build failures in future versions of Flutter. 
+Please migrate your app to Built-in Kotlin using this guide: https://docs.flutter.dev/release/breaking-changes/migrate-to-built-in-kotlin/for-app-developers
+2
+
+FAILURE: Build failed with an exception.
+
+* What went wrong:
+Execution failed for task ':app:configureCMakeDebug[x86_64]'.
+*/
 
